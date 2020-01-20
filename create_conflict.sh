f68fe751d332d195d4ab4c9a3140e017467dd422
Martin Skarzynski <marskar@gmail.com>
2020-01-20 18:39:46 -0500

    add additional shell scripts

---
+++ create_conflict.sh
@@ -0,0 +1,7 @@
+git checkout -b another_branch
+echo "echo 'Hello World'" > my_code.sh
+git commit -am 'first commit on another_branch'
+git checkout master
+echo "echo 'Hello World!'" > my_code.sh
+git commit -am 'second commit on master'
+git merge another_branch
